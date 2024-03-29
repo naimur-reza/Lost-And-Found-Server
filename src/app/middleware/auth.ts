3d01ddb2b4ce7541e92981ca6998d787a6508c01
Naimur Reza <naimur.rezaa@gmail.com>
2024-03-30 02:54:19 +0600

    Add found item module with global user type

--- src/app/middleware/auth.ts
+++ src/app/middleware/auth.ts
@@ -18,6 +18,8 @@ const auth = () => {
       },
     });
 
+    req.user = user;
+
     next();
   });
 };
---
+++ src/app/modules/FoundItem/foundItem.validation.ts
@@ -0,0 +1,14 @@
+import { object, string } from "zod";
+
+const foundItemSchema = object({
+  body: object({
+    categoryId: string().uuid().optional(),
+    foundItemName: string().min(1, { message: "foundItemName is required." }),
+    description: string().min(1, { message: "description is required." }),
+    location: string().min(1, { message: "location is required." }),
+  }),
+});
+
+export const foundItemValidationSchema = {
+  foundItemSchema,
+};
